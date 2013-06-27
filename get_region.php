993e815c26c01affe95d00a5346771de78a52c7d
Czachur <nathan.czachur@gmail.com>
2013-06-27 16:39:28 +0100

    Added get_region.php
    A function to return a region or error based on a post code input
    
    Hard-coded region_codes.xml

---
+++ get_region.php
@@ -0,0 +1,29 @@
+function get_region($post_code) {
+    if ($post_code === '') {
+        return "No post code entered.";
+    } else {
+        $post_region = -1;
+
+        $post_code = strtoupper(str_replace(" ", "", $post_code));
+
+        $post_code_array = str_split($post_code);
+
+        $xml = simplexml_load_file("region_codes.xml");
+
+        if (isset($post_code_array[1]) && is_numeric($post_code_array[1])) {
+            $post_region = $post_code[0];
+        } elseif (isset($post_code_array[2]) && is_numeric($post_code_array[2])) {
+            $post_region = ($post_code[0] . $post_code[1]);
+        }
+
+        if ($post_region == -1) {
+            return "Invalid postal code.";
+        } else {
+            foreach ($xml as $record) {
+                if ($record->code == $post_region) {
+                    return $record->region;
+                }
+            }
+        }
+    }
+}
